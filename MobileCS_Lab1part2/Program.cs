4da5cd4fa16f85075141d6a4b75a095bfd11c5ab
stepan <stepan-alekseev-87@mail.ru>
2020-10-25 20:46:14 +0700

    first commit

---
+++ MobileCS_Lab1part2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileCS_Lab1part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there");
+            //Console.ReadLine();
+        }
+    }
+}
